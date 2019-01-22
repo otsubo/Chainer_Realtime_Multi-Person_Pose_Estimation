b3b8a8be754af949494d9c3feffcc7c34f841577
Satoshi Otsubo <otsubo@jsk.imi.i.u-tokyo.ac.jp>
2019-01-22 21:11:10 +0900

    tmp commit

--- cloth_data_loader.py
+++ cloth_data_loader.py
@@ -1,14 +1,21 @@
 # coding:utf-8
-import json
 import os
+import os.path as osp
+
+import chainer
+import json
+import numpy as np
+import scipy.misc
+from sklearn.model_selection import train_test_split
+
 import sys
+
+import chainer
+from chainer.dataset import DatasetMixin
+
 import cv2
 import math
 import random
-import numpy as np
-
-from chainer.dataset import DatasetMixin
-#from pycocotools.coco import COCO
 
 from entity import JointType, params
 
@@ -32,15 +39,16 @@ class ClothDataLoader(DatasetMixin):
     ]
     mean_bgr = np.array((104.00698793, 116.66876762, 122.67891434))
 
-    def __init__(self, split, retrun_images=False, return_all=False):
+    #def __init__(self, split, retrun_image=False, return_all=False):
+    def __init__(self, split):
         #assert mode in ['train', 'val', 'eval'], 'Data loading mode is invalid.'
         assert split in ('train', 'val')
         ids = self._get_ids()
         iter_train, iter_val = train_test_split(
             ids, test_size= 0.2, random_state=np.random.RandomState(1234))
-        self.ids = iter_train if split == 'tarin' else = iter_val
-        self._return_imga = return_image
-        self._return_all = return_all
+        self.ids = iter_train if split == 'tarin' else iter_val
+        #self._return_image = return_image
+        #self._return_all = return_all
 
     def __len__(self):
         return len(self.ids)
@@ -60,18 +68,6 @@ class ClothDataLoader(DatasetMixin):
         datum -= self.mean_bgr
         datum = datum.transpose((2, 0, 1))
         return datum
-
-    def json_file_to_lbl(self, img_shape, json_file):
-        lable_name_to_value = {}
-        for label_value, label_name in enumerate(self.class_names):
-            label_name_to_value[label_name] = label_value
-        with open(json_file) as f:
-            data = json.load(f)
-        lbl = labelme.utils.shape_to_label(
-            img_shape, data['shapes'], label_name_to_value
-        )
-        return lbl
-
 
     def overlay_paf(self, img, paf):
         hue = ((np.arctan2(paf[1], paf[0]) / np.pi) / -2 + 0.5)
@@ -100,156 +96,6 @@ class ClothDataLoader(DatasetMixin):
         rgb_heatmap = cv2.applyColorMap((heatmap * 255).astype(np.uint8), cv2.COLORMAP_JET)
         img = cv2.addWeighted(img, 0.6, rgb_heatmap, 0.4, 0)
         return img
-
-    def overlay_ignore_mask(self, img, ignore_mask):
-        img = img * np.repeat((ignore_mask == 0).astype(np.uint8)[:, :, None], 3, axis=2)
-        return img
-
-    def get_pose_bboxes(self, poses):
-        pose_bboxes = []
-        for pose in poses:
-            x1 = pose[pose[:, 2] > 0][:, 0].min()
-            y1 = pose[pose[:, 2] > 0][:, 1].min()
-            x2 = pose[pose[:, 2] > 0][:, 0].max()
-            y2 = pose[pose[:, 2] > 0][:, 1].max()
-            pose_bboxes.append([x1, y1, x2, y2])
-        pose_bboxes = np.array(pose_bboxes)
-        return pose_bboxes
-
-    def resize_data(self, img, ignore_mask, poses, shape):
-        """resize img, mask and annotations"""
-        img_h, img_w, _ = img.shape
-
-        resized_img = cv2.resize(img, shape)
-        ignore_mask = cv2.resize(ignore_mask.astype(np.uint8), shape).astype('bool')
-        poses[:, :, :2] = (poses[:, :, :2] * np.array(shape) / np.array((img_w, img_h)))
-        return resized_img, ignore_mask, poses
-
-    def random_resize_img(self, img, ignore_mask, poses):
-        h, w, _ = img.shape
-        joint_bboxes = self.get_pose_bboxes(poses)
-        bbox_sizes = ((joint_bboxes[:, 2:] - joint_bboxes[:, :2] + 1)**2).sum(axis=1)**0.5
-
-        min_scale = params['min_box_size']/bbox_sizes.min()
-        max_scale = params['max_box_size']/bbox_sizes.max()
-
-        # print(len(bbox_sizes))
-        # print('min: {}, max: {}'.format(min_scale, max_scale))
-
-        min_scale = min(max(min_scale, params['min_scale']), 1)
-        max_scale = min(max(max_scale, 1), params['max_scale'])
-
-        # print('min: {}, max: {}'.format(min_scale, max_scale))
-
-        scale = float((max_scale - min_scale) * random.random() + min_scale)
-        shape = (round(w * scale), round(h * scale))
-
-        # print(scale)
-
-        resized_img, resized_mask, resized_poses = self.resize_data(img, ignore_mask, poses, shape)
-        return resized_img, resized_mask, poses
-
-    def random_rotate_img(self, img, mask, poses):
-        h, w, _ = img.shape
-        # degree = (random.random() - 0.5) * 2 * params['max_rotate_degree']
-        degree = np.random.randn() / 3 * params['max_rotate_degree']
-        rad = degree * math.pi / 180
-        center = (w / 2, h / 2)
-        R = cv2.getRotationMatrix2D(center, degree, 1)
-        bbox = (w*abs(math.cos(rad)) + h*abs(math.sin(rad)), w*abs(math.sin(rad)) + h*abs(math.cos(rad)))
-        R[0, 2] += bbox[0] / 2 - center[0]
-        R[1, 2] += bbox[1] / 2 - center[1]
-        rotate_img = cv2.warpAffine(img, R, (int(bbox[0]+0.5), int(bbox[1]+0.5)), flags=cv2.INTER_CUBIC,
-                                    borderMode=cv2.BORDER_CONSTANT, borderValue=[127.5, 127.5, 127.5])
-        rotate_mask = cv2.warpAffine(mask.astype('uint8')*255, R, (int(bbox[0]+0.5), int(bbox[1]+0.5))) > 0
-
-        tmp_poses = np.ones_like(poses)
-        tmp_poses[:, :, :2] = poses[:, :, :2].copy()
-        tmp_rotate_poses = np.dot(tmp_poses, R.T)  # apply rotation matrix to the poses
-        rotate_poses = poses.copy()  # to keep visibility flag
-        rotate_poses[:, :, :2] = tmp_rotate_poses
-        return rotate_img, rotate_mask, rotate_poses
-
-    def random_crop_img(self, img, ignore_mask, poses):
-        h, w, _ = img.shape
-        #insize = self.insize
-        joint_bboxes = self.get_pose_bboxes(poses)
-        bbox = random.choice(joint_bboxes)  # select a bbox randomly
-        bbox_center = bbox[:2] + (bbox[2:] - bbox[:2])/2
-
-        r_xy = np.random.rand(2)
-        perturb = ((r_xy - 0.5) * 2 * params['center_perterb_max'])
-        center = (bbox_center + perturb + 0.5).astype('i')
-
-        crop_img = np.zeros((insize, insize, 3), 'uint8') + 127.5
-        crop_mask = np.zeros((insize, insize), 'bool')
-
-        offset = (center - (insize-1)/2 + 0.5).astype('i')
-        offset_ = (center + (insize-1)/2 - (w-1, h-1) + 0.5).astype('i')
-
-        x1, y1 = (center - (insize-1)/2 + 0.5).astype('i')
-        x2, y2 = (center + (insize-1)/2 + 0.5).astype('i')
-
-        x1 = max(x1, 0)
-        y1 = max(y1, 0)
-        x2 = min(x2, w-1)
-        y2 = min(y2, h-1)
-
-        x_from = -offset[0] if offset[0] < 0 else 0
-        y_from = -offset[1] if offset[1] < 0 else 0
-        x_to = insize - offset_[0] - 1 if offset_[0] >= 0 else insize - 1
-        y_to = insize - offset_[1] - 1 if offset_[1] >= 0 else insize - 1
-
-        crop_img[y_from:y_to+1, x_from:x_to+1] = img[y1:y2+1, x1:x2+1].copy()
-        crop_mask[y_from:y_to+1, x_from:x_to+1] = ignore_mask[y1:y2+1, x1:x2+1].copy()
-
-        poses[:, :, :2] -= offset
-        return crop_img.astype('uint8'), crop_mask, poses
-
-    def distort_color(self, img):
-        img_max = np.broadcast_to(np.array(255, dtype=np.uint8), img.shape[:-1])
-        img_min = np.zeros(img.shape[:-1], dtype=np.uint8)
-
-        hsv_img = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2HSV).astype(np.int32)
-        hsv_img[:, :, 0] = np.maximum(np.minimum(hsv_img[:, :, 0] - 10 + np.random.randint(20 + 1), img_max), img_min) # hue
-        hsv_img[:, :, 1] = np.maximum(np.minimum(hsv_img[:, :, 1] - 40 + np.random.randint(80 + 1), img_max), img_min) # saturation
-        hsv_img[:, :, 2] = np.maximum(np.minimum(hsv_img[:, :, 2] - 30 + np.random.randint(60 + 1), img_max), img_min) # value
-        hsv_img = hsv_img.astype(np.uint8)
-
-        distorted_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
-        return distorted_img
-
-    def flip_img(self, img, mask, poses):
-        flipped_img = cv2.flip(img, 1)
-        flipped_mask = cv2.flip(mask.astype(np.uint8), 1).astype('bool')
-        poses[:, :, 0] = img.shape[1] - 1 - poses[:, :, 0]
-
-        def swap_joints(poses, joint_type_1, joint_type_2):
-            tmp = poses[:, joint_type_1].copy()
-            poses[:, joint_type_1] = poses[:, joint_type_2]
-            poses[:, joint_type_2] = tmp
-
-        swap_joints(poses, JointType.LeftEye, JointType.RightEye)
-        swap_joints(poses, JointType.LeftEar, JointType.RightEar)
-        swap_joints(poses, JointType.LeftShoulder, JointType.RightShoulder)
-        swap_joints(poses, JointType.LeftElbow, JointType.RightElbow)
-        swap_joints(poses, JointType.LeftHand, JointType.RightHand)
-        swap_joints(poses, JointType.LeftWaist, JointType.RightWaist)
-        swap_joints(poses, JointType.LeftKnee, JointType.RightKnee)
-        swap_joints(poses, JointType.LeftFoot, JointType.RightFoot)
-        return flipped_img, flipped_mask, poses
-
-    def augment_data(self, img, ignore_mask, poses):
-        aug_img = img.copy()
-        aug_img, ignore_mask, poses = self.random_resize_img(aug_img, ignore_mask, poses)
-        aug_img, ignore_mask, poses = self.random_rotate_img(aug_img, ignore_mask, poses)
-        aug_img, ignore_mask, poses = self.random_crop_img(aug_img, ignore_mask, poses)
-        if np.random.randint(2):
-            aug_img = self.distort_color(aug_img)
-        if np.random.randint(2):
-            aug_img, ignore_mask, poses = self.flip_img(aug_img, ignore_mask, poses)
-
-        return aug_img, ignore_mask, poses
 
     # return shape: (height, width)
     def generate_gaussian_heatmap(self, shape, joint, sigma):
@@ -314,59 +160,19 @@ class ClothDataLoader(DatasetMixin):
             pafs = np.vstack((pafs, paf))
         return pafs.astype('f')
 
-    #todo: remove imgids and write loader for labelme which can load json file
-    def get_img_annotation(self, ind=None, img_id=None):
-        annotations = None
-
-        if ind is not None:
-            img_id = self.imgIds[ind]
-        anno_ids = self.coco.getAnnIds(imgIds=[img_id], iscrowd=None)
-
-        # annotation for that image
-        if len(anno_ids) > 0:
-            annotations_for_img = self.coco.loadAnns(anno_ids)
-
-            person_cnt = 0
-            valid_annotations_for_img = []
-            for annotation in annotations_for_img:
-                # if too few keypoints or too small
-                if annotation['num_keypoints'] >= params['min_keypoints'] and annotation['area'] > params['min_area']:
-                    person_cnt += 1
-                    valid_annotations_for_img.append(annotation)
-
-            # if person annotation
-            if person_cnt > 0:
-                annotations = valid_annotations_for_img
-
-        img_path = os.path.join(params['coco_dir'], 'train2017', self.coco.loadImgs([img_id])[0]['file_name'])
-        #img_path = os.path.join(params['coco_dir'], 'train2017', self.coco.loadImgs([img_id])[0]['file_name'])
-        mask_path = os.path.join(params['coco_dir'], 'ignore_mask_train2017', '{:012d}.png'.format(img_id))
-
-        img = cv2.imread(img_path)
-        ignore_mask = cv2.imread(mask_path, 0)
-        if ignore_mask is None:
-            ignore_mask = np.zeros(img.shape[:2], 'bool')
-        else:
-            ignore_mask = ignore_mask == 255
-
-        if self.mode == 'eval':
-            return img, img_id, annotations_for_img, ignore_mask
-        return img, img_id, annotations, ignore_mask
-
     #todo: 
-    def generate_labels(self, img, poses, ignore_mask):
-        img, ignore_mask, poses = self.augment_data(img, ignore_mask, poses)
-        resized_img, ignore_mask, resized_poses = self.resize_data(img, ignore_mask, poses, shape=(self.insize, self.insize))
+    def generate_labels(self, img, pose):
+        #img, ignore_mask, poses = self.augment_data(img, ignore_mask, poses)
+        #resized_img, ignore_mask, resized_poses = self.resize_data(img, ignore_mask, poses, shape=(self.insize, self.insize))
 
-        heatmaps = self.generate_heatmaps(img, poses, params['heatmap_sigma'])
-        pafs = self.generate_pafs(img, poses, params['paf_sigma'])
-        ignore_mask = cv2.morphologyEx(ignore_mask.astype('uint8'), cv2.MORPH_DILATE, np.ones((16, 16))).astype('bool')
-        return img, pafs, heatmaps, ignore_mask
+        heatmaps = self.generate_heatmaps(img, pose, params['heatmap_sigma'])
+        pafs = self.generate_pafs(img, pose, params['paf_sigma'])
+        return img, pafs, heatmaps
 
     def shapes_to_pose(self, json_path):
         pose = []
         pose_tmp = []
-        json_file = ops.join(json_path, 'image.json')
+        json_file = osp.join(json_path, 'image.json')
         pose = np.zeros((0, len(JointType), 3), dtype=np.int32)
         with open(json_file) as f:
             data = json.load(f)
@@ -385,52 +191,28 @@ class ClothDataLoader(DatasetMixin):
         dataset_dir = chainer.dataset.get_dataset_directory(
             'cloth_estimation/ClothV1')
 
-        img_file = ops.join(dataset_dir,data_id, 'imgage.png')
+        img_file = osp.join(dataset_dir,data_id, 'image.png')
         img = scipy.misc.imread(img_file)
-        img_datum = self.img_to_datum(img_file)
-
-        json_file = osp.join(dataset_dir, data_id, 'image.json')
+        img_datum = self.img_to_datum(img)
         img = np.array(img)
-        lbl = np.array(lbl)
+
+        json_file = osp.join(dataset_dir, data_id)
+        pose = self.shapes_to_pose(json_file)
 
         #todo: calculate pafs, heatmaps and return them
-        return img, lbl
-
-        while annotations is None:
-            img_id = self.imgIds[np.random.randint(len(self))]
-            img, img_id, annotations, ignore_mask = self.get_img_annotation(img_id=img_id)
-
         #poses = self.parse_coco_annotation(annotations)
-        poses = self.shapes_to_pose(annotations)
-        resized_img, pafs, heatmaps, ignore_mask = self.generate_labels(img, poses, ignore_mask)
-        return resized_img, pafs, heatmaps, ignore_mask
+        img, pafs, heatmaps = self.generate_labels(img, pose)
+        return img, pafs, heatmaps
 
-if __name__ == '__main__':
-    mode = 'train'
-    #coco = COCO(os.path.join(params['coco_dir'], 'annotations/person_keypoints_{}2017.json'.format(mode)))
-    data_loader = ClothDataLoader(datapath, params['insize'], mode=mode)
-
-    cv2.namedWindow('w', cv2.WINDOW_NORMAL)
-
-    for i in range(len(data_loader)):
-        img, img_id, annotations, ignore_mask = data_loader.get_img_annotation(ind=i)
-        if annotations is not None:
-            poses = data_loader.parse_coco_annotation(annotations)
-            resized_img, pafs, heatmaps, ignore_mask = data_loader.generate_labels(img, poses, ignore_mask)
-
-            # resize to view
-            shape = (params['insize'],) * 2
-            pafs = cv2.resize(pafs.transpose(1, 2, 0), shape).transpose(2, 0, 1)
-            heatmaps = cv2.resize(heatmaps.transpose(1, 2, 0), shape).transpose(2, 0, 1)
-            ignore_mask = cv2.resize(ignore_mask.astype(np.uint8)*255, shape) > 0
-
-            # overlay labels
-            img_to_show = resized_img.copy()
-            img_to_show = data_loader.overlay_pafs(img_to_show, pafs)
-            img_to_show = data_loader.overlay_heatmap(img_to_show, heatmaps[:-1].max(axis=0))
-            img_to_show = data_loader.overlay_ignore_mask(img_to_show, ignore_mask)
-
-            cv2.imshow('w', np.hstack((resized_img, img_to_show)))
-            k = cv2.waitKey(0)
-            if k == ord('q'):
-                sys.exit()
+if __name__ =='__main__':
+    import matplotlib.pyplot as plt
+    dataset = ClothDataLoader('train')
+    for i in range(len(dataset)):
+        img, pafs, heatmaps = dataset.get_example(i)
+        print(img)
+        print(pafs)
+        print(heatmaps)
+    # for i in range(len(dataset)):
+    #     label_viz = hogehoge #todo
+    #     plt.subplot((221))
+    #     plt.show()
